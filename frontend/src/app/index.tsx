643aee6557db89cc9af7e7de8f0acedf11c9a9cf
Oleg Skarednov <olegskar1@gmail.com>
2023-06-23 22:04:07 +0500

    fix: AppRouter

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,16 +1,18 @@
-import { AppRouter } from './providers/RouterProvider/ui/AppRouter';
+import { Outlet } from 'react-router-dom';
+
 import { withTheme } from './providers/ThemeProvider/hoc/withTheme';
 
 import { MainLayout } from '@/shared/layouts';
 import { useTheme } from '@/shared/lib/hooks/useTheme';
 import { Sidebar } from '@/widgets/Sidebar';
+
 import './styles/index.scss';
 
 const App = () => {
   const { theme } = useTheme();
   return (
     <div id='app' className={theme}>
-      <MainLayout content={<AppRouter />} header={<p>header</p>} sidebar={<Sidebar />} toolbar={<p>toolbar</p>} />
+      <MainLayout content={<Outlet />} header={<p>header</p>} sidebar={<Sidebar />} toolbar={<p>toolbar</p>} />
     </div>
   );
 };
--- frontend/src/app/providers/RouterProvider/ui/AppRouter.tsx
+++ frontend/src/app/providers/RouterProvider/ui/AppRouter.tsx
@@ -1,9 +1,25 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Suspense, useCallback } from 'react';
+import { Route, RouteObject, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 
+import App from '../../../index';
 import { routeConfig } from '../config/routeConfig';
 
-const router = createBrowserRouter(Object.values(routeConfig));
-
 export const AppRouter = () => {
+  const renderWithWrapper = useCallback((route: RouteObject) => {
+    const element = <Suspense>{route.element}</Suspense>;
+
+    return <Route element={element} path={route.path} key={route.path} />;
+  }, []);
+
+  const childrenRoutes = Object.values(routeConfig).map(renderWithWrapper);
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<App />}>
+        {childrenRoutes}
+      </Route>
+    )
+  );
+
   return <RouterProvider router={router} />;
 };
